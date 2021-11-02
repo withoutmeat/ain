b855fe4e4892a7e3d514dd9dca314e1333e2d7d5
withoutmeat <withoutmeat@qq.com>
2021-11-02 19:13:11 +0800

    feat: windicss

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+// windicss layers
+import "virtual:windi-base.css";
+import "virtual:windi-components.css";
+// your custom styles here
+import "./styles/main.css";
+// windicss utilities should be the last style import
+import "virtual:windi-utilities.css";
+// windicss devtools support (dev only)
+import "virtual:windi-devtools";
+
+createApp(App).mount("#app");
---
+++ windi.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite-plugin-windicss";
+
+export default defineConfig({
+  extract: {
+    include: ["src/**/*.{vue,html,jsx,tsx}"],
+    exclude: ["node_modules", ".git"],
+  },
+});
